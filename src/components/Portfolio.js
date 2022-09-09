c2eb681dece5cabdde5f8bcec1976599d46770e7
unknown <nhan.nguyenkieu317@gmail.com>
2022-09-09 12:29:15 +0700

    fiveth commit

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -23,18 +23,18 @@ export default class Portfolio extends Component {
                             <iframe width="550" height="375" src="https://www.youtube.com/embed/FOOEn4aIbM8" title="vnhp online aution" frameBorder="4" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>                       
                         </div>
                         <div className="col-md-6">
-                            <h4 className='text-center'>My mission in <span className='text-danger'>Online Auction Project</span></h4>
+                            <h4 className='text-center'>Missions in <span className='text-danger'>Online Auction Project</span></h4>
                             <div data-spy="scroll" className='pre-scrollable' data-target="#myScrollspy" data-offset="10">
                                 <h5>ADMIN PAGE</h5>
                                 <h6>FUNCTION</h6>
-                                <p className='mb-0'>-<span className='text-danger'> Login/Logout:</span> Client won't be able to access any admin page if they aren't logged in.</p>
-                                <p className='mb-0'>-<span className='text-danger'> Admin profile:</span> Display admin information. Client also change their information and password.</p>
-                                <p className='mb-0'>-<span className='text-danger'> Dasboard:</span> I have gotten data from API and have displayed revenues by charts and statistic of loyal customer.</p>
-                                <p className='mb-0'>-<span className='text-danger'> Customer:</span>  Display all customer by datatable. Client also see user's bidding histories and all payment histories</p>
-                                <p className='mb-0'>-<span className='text-danger'> Feedback:</span>  Each card for one product, you can click on comment to show all comments of that product.</p>
-                                <p className='mb-0'>-<span className='text-danger'> Bill:</span> I have displayed all bills of all customers from API, client have searched text so client can search to look for which bill they need. Besides, I also have displayed 3 statuses of bill (Paid + Unpaid + Pending Payment).</p>
-                                <p className='mb-0'>-<span className='text-danger'> Bill Detail:</span> Client can click on detail button to view Bill Detail and can call for customer remind their payment or other issuess.</p>
-                                <p className='mb-0'>-<span className='text-danger'> Category:</span> Displayed all categories of website from API, client also can active or deactive one category. It will show up or disappear on user page. Besides, client can add new or edit any categoríe they want</p>
+                                <p className='mb-0'>-<span className='text-danger'> Login/Logout:</span> Client won't be able to access any admin pages if they aren't logged in.</p>
+                                <p className='mb-0'>-<span className='text-danger'> Admin profile:</span> Display admin information. Client also changes their information and passwords.</p>
+                                <p className='mb-0'>-<span className='text-danger'> Dasboard:</span> I have taken data from API and have displayed revenues by charts and statistic of loyal customer.</p>
+                                <p className='mb-0'>-<span className='text-danger'> Customer:</span> Display all customer by a datatable. Client also sees user's bidding histories and all payment histories</p>
+                                <p className='mb-0'>-<span className='text-danger'> Feedback:</span> Each card for one product, you can click on comment to show all product's review.</p>
+                                <p className='mb-0'>-<span className='text-danger'> Bill:</span> I have displayed all bills of all customers from API, client has searched characters so client can search to look for which bill they need. Besides, I also have displayed 3 statuses of bill (Paid + Unpaid + Pending Payment).</p>
+                                <p className='mb-0'>-<span className='text-danger'> Bill Detail:</span> Client can click on the detail button to view Bill Detail and call for customer to remind their payments or other issues.</p>
+                                <p className='mb-0'>-<span className='text-danger'> Category:</span>  All categories are display on the website from API, client can activate or deactivate any categories. It can show up or disappear flexibly on user's page. Moreover, client can add or edit any categories they want</p>
                                 <p className='mb-3'>-<span className='text-danger'> Product:</span> Displayed all products of website from API, client can also active or deactive one product. It will show up or disappear on user page. Besides, client can add new or edit any products they want</p>
                                 <h6>SOFTWARE</h6>
                                 <p className='mb-0'>- Visual studio code 2019</p>
@@ -53,18 +53,18 @@ export default class Portfolio extends Component {
                             <iframe width="550" height="360" src="https://www.youtube.com/embed/JYM16JwFCSU" title="vnhp health care" frameBorder="4" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                         </div>
                         <div className="col-md-6">
-                            <h4 className='text-center'>My mission in <span className='text-danger'>Personal Health Assistant</span></h4>
+                            <h4 className='text-center'>Missions in <span className='text-danger'>Personal Health Assistant</span></h4>
                             <div data-spy="scroll" className='pre-scrollable' data-target="#myScrollspy" data-offset="10">
                                 <h5>ADMIN PAGE</h5>
                                 <h6>FUNCTION</h6>
                                 <p className='mb-0'>-<span className='text-danger'> Login/Logout:</span> Client won't be able to access any admin page if they aren't logged in.</p>
-                                <p className='mb-0'>-<span className='text-danger'> Admin profile:</span> Display admin information. Client also change their information and password.</p>
-                                <p className='mb-0'>-<span className='text-danger'> Dasboard:</span> I have gotten data from API and have displayed revenues by charts and statistic of loyal customer.</p>
-                                <p className='mb-0'>-<span className='text-danger'> Customer:</span>  Display all customer by datatable. Client also see user's bidding histories and all payment histories</p>
-                                <p className='mb-0'>-<span className='text-danger'> Feedback:</span>  Each card for one product, you can click on comment to show all comments of that product.</p>
-                                <p className='mb-0'>-<span className='text-danger'> Product:</span> Displayed all products of website from API, client can also active or deactive one product. It will show up or disappear on user page. Besides, client can add new or edit any products they want</p>
-                                <p className='mb-0'>-<span className='text-danger'> Doctor:</span> Displayed all doctors of website from API. Besides, client can add new doctors</p>
-                                <p className='mb-0'>-<span className='text-danger'> Post:</span> Client can add new post about heathing of website. Besides, client can add new or edit any post want</p>
+                                <p className='mb-0'>-<span className='text-danger'> Admin profile:</span> Display admin information. Client also changes their information and passwords.</p>
+                                <p className='mb-0'>-<span className='text-danger'> Dasboard:</span> I have taken data from API and have displayed revenues by charts and statistic of loyal customer.</p>
+                                <p className='mb-0'>-<span className='text-danger'> Customer:</span> Display all customer by a datatable. Client also sees all user's bidding and payment histories</p>
+                                <p className='mb-0'>-<span className='text-danger'> Feedback:</span> Present each card for one product, you can click on comments to show all product's review.</p>
+                                <p className='mb-0'>-<span className='text-danger'> Product:</span> Displayed all products of website from API, client can also activate or deactivate one product. It will show up or disappear on user page. Besides, client can add or edit any products they want</p>
+                                <p className='mb-0'>-<span className='text-danger'> Doctor:</span> Displayed all doctors of website from API. Moreover, client can add new doctors</p>
+                                <p className='mb-0'>-<span className='text-danger'> Post:</span> Client can add new post about heathing of website. Besides, client can add new or edit any posts they want</p>
                                 <p className='mb-3'>-<span className='text-danger'> Appointment:</span> Display all current and histories appointment</p>
                                 <h6>SOFTWARE</h6>
                                 <p className='mb-0'>- Visual studio code 2019</p>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -22,15 +22,15 @@ export default class Resume extends Component {
                         <div className="card-body">
                             <h6 className="title text-danger">2021 - Present</h6>
                             <p>Software Engineering</p>
-                            <p className="subtitle">I'm studying at Aptech Computer Education to get Advance Diploma in Software Engineering, i have finished web development certificate</p>
+                            <p className="subtitle">I'm studying at Aptech Computer Education to get Advance Diploma in Software Engineering, I have finished web development certificate</p>
                             <hr/>
                             <h6 className="title text-danger">2016 - 2018</h6>
                             <p>Construction Engineering</p>
-                            <p className="subtitle">I have learnt in Saigon Technology University gor 2 years and a student of Construction Engineering, then i decided not to study anymore and working in the F&B industry</p>
+                            <p className="subtitle">I have learnt in Saigon Technology University gor 2 years and a student of Construction Engineering, then I decided not to study anymore and worked in the F&B industry</p>
                             <hr/>
                             <h6 className="title text-danger">2015 - 2016</h6>
                             <p>High School Degree</p>
-                            <p className="subtitle">Graduated at Ngo Gia Tu highschool, then took a year to unskilled labor, i have begun university at 2016</p>
+                            <p className="subtitle">Graduating at Ngo Gia Tu highschool, then took a year to unskilled labor, i have begun university at 2016</p>
                         </div>
                     </div>
                 </div>
